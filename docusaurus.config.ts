7d94227a1d2a41205531f4402674798d56f2e4fa
Reinaldo Assis <randroi75@gmail.com>
2024-10-24 05:31:52 -0300

    fix: missing build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'reinaldoassis', // Usually your GitHub org/user name.
   projectName: 'wims-doc', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   deploymentBranch: "gh-pages",
 
